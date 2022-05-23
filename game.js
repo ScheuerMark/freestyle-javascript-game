511c1320004197d6a67b7b34cb3dfb109c47cc52
Attila Nagy <nagyattila86.hu@gmail.com>
2022-05-23 14:12:07 +0200

    feat: create start board

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
 initGame();
 
-function initGame() {
+function createBoard(width=9) {
+    const gameField = document.getElementsByClassName('game-field');
+    for (let i=0; i < Math.pow(width, 2); i++) {
+        const field = document.createElement('div');
+        field.classList.add('field');
+        gameField[0].appendChild(field);
+    }
+}
 
+function initGame() {
+    createBoard();
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
